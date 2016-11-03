014dc7d5f5fb13744af1ba6a502cb3636c7fd3d0
pdebelak <pdebelak@gmail.com>
2016-11-03 06:30:06 -0500

    Add tests for nav link

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,18 @@
 defmodule MemoWeb.LayoutViewTest do
   use MemoWeb.ConnCase, async: true
+
+  alias MemoWeb.LayoutView
+  import Phoenix.HTML, only: [safe_to_string: 1]
+
+  test "nav_link is not active when not on that page" do
+    link = LayoutView.nav_link(%{request_path: "/other_path"}, "Link text", "/path")
+    |> safe_to_string()
+    refute String.match? link, ~r/is-active/
+  end
+
+  test "nav_link is active when on that page" do
+    link = LayoutView.nav_link(%{request_path: "/path"}, "Link text", "/path")
+    |> safe_to_string()
+    assert String.match? link, ~r/is-active/
+  end
 end
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,8 +1,8 @@
 defmodule MemoWeb.LayoutView do
   use MemoWeb.Web, :view
 
-  def nav_link(conn, text, path) do
-    link text, to: path, class: nav_link_class(conn.request_path, path)
+  def nav_link(%{request_path: request_path}, text, path) do
+    link text, to: path, class: nav_link_class(request_path, path)
   end
 
   defp nav_link_class(path, path), do: "nav-item is-active"
